17d549dd6bfac37e23046a64e80f461e63b9b961
Edmond Chiu <email.edmond.chiu@gmail.com>
2022-10-21 17:24:01 -0400

    layout

---
+++ src/main/java/ca/echiu/controller/NavigatorController.java
@@ -0,0 +1,11 @@
+package ca.echiu.controller;
+
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+
+@Component
+@FxmlView("/fxml/Navigator.fxml")
+public class NavigatorController{
+
+
+}
