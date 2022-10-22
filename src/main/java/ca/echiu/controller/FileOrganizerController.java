0c7a20b579a4db922d5f50f8358de3c20512f6db
Edmond Chiu <email.edmond.chiu@gmail.com>
2022-10-22 16:59:28 -0400

    include file organizer section in Main Window

---
+++ src/main/java/ca/echiu/controller/FileOrganizerController.java
@@ -0,0 +1,11 @@
+package ca.echiu.controller;
+
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+
+@Component
+@FxmlView("fxml/FileOrganizer.fxml")
+public class FileOrganizerController {
+
+
+}
