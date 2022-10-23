e397c8e1fd0bcbd91277fa5a9eaa6b34760b8b25
Edmond Chiu <email.edmond.chiu@gmail.com>
2022-10-22 22:01:52 -0400

    include review components

---
+++ src/main/java/ca/echiu/controller/ReviewController.java
@@ -0,0 +1,9 @@
+package ca.echiu.controller;
+
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+
+@Component
+@FxmlView("fxml/Review.fxml")
+public class ReviewController {
+}
