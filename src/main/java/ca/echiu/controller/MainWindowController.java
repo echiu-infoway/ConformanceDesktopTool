85eb46529ea1b0fe642914d69e4dde4ce619a01c
Edmond Chiu <email.edmond.chiu@gmail.com>
2022-10-22 21:37:16 -0400

    toggle button to show and hide file organizer

--- src/main/java/ca/echiu/controller/MainWindowController.java
+++ src/main/java/ca/echiu/controller/MainWindowController.java
@@ -5,6 +5,8 @@ import ca.echiu.service.WeatherService;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.layout.VBox;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,6 +16,10 @@ import org.springframework.stereotype.Component;
 public class MainWindowController {
     @FXML
     private Label weatherLabel;
+    @FXML
+    private VBox fileOrganizerVBox;
+    @FXML
+    private ToggleButton reviewToggleButton;
     @Autowired
     private WeatherService weatherService;
     @Autowired
@@ -22,5 +28,16 @@ public class MainWindowController {
 
     public void loadWeatherForecast(ActionEvent actionEvent) {
         weatherLabel.setText(weatherService.getWeatherForecast());
+    }
+
+    public void toggleFileOrganizerVbox(){
+        if(reviewToggleButton.isSelected())
+        {
+            fileOrganizerVBox.setVisible(false);
+        }
+        if(!reviewToggleButton.isSelected()){
+            fileOrganizerVBox.setVisible(true);
+        }
+
     }
 }
