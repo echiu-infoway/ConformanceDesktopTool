e5d5cb62c9833f1156b62600893f9a12e510e53e
Edmond Chiu <email.edmond.chiu@gmail.com>
2022-10-23 07:42:30 -0400

    include table view for review

--- src/main/java/ca/echiu/controller/ReviewController.java
+++ src/main/java/ca/echiu/controller/ReviewController.java
@@ -1,9 +1,20 @@
 package ca.echiu.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
 
 @Component
 @FxmlView("fxml/Review.fxml")
 public class ReviewController {
+
+    @FXML
+    private TableView reviewTableView;
+
+    @FXML
+    public void initialize(){
+
+    }
 }
