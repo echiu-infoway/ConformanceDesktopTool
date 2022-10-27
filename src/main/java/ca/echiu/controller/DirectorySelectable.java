4cd7d940d8f1a461710b887bff697b47eb6a84ee
Edmond Chiu <email.edmond.chiu@gmail.com>
2022-10-27 06:53:41 -0400

    refactor

--- src/main/java/ca/echiu/controller/DirectorySelectable.java
+++ src/main/java/ca/echiu/controller/DirectorySelectable.java
@@ -2,8 +2,8 @@ package ca.echiu.controller;
 
 public interface DirectorySelectable {
 
-    public final String NO_APPLICABLE_FILES_FOUND = "No applicable files found";
-    public final String TOTAL_NUMBER_OF_FILES = "TOTAL NUMBER OF FILES: ";
+    String NO_APPLICABLE_FILES_FOUND = "No applicable files found";
+    String TOTAL_NUMBER_OF_FILES = "TOTAL NUMBER OF FILES: ";
 
     public void chooseDirectory();
 
